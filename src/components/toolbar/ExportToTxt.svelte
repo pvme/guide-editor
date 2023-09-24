80a5ade05b94230a144b6b9e6317b5f025916c26
Rcm37 <46052381+Rcm37@users.noreply.github.com>
2023-09-24 11:13:44 -0400

    indent and button style

--- src/components/toolbar/ExportToTxt.svelte
+++ src/components/toolbar/ExportToTxt.svelte
@@ -6,11 +6,11 @@
       hiddenElement.href = 'data:attachment/text,' + encodeURI($text);
       hiddenElement.target = '_blank';
       hiddenElement.download = 'GuideEditorExport.txt';
-	  hiddenElement.click();
+	    hiddenElement.click();
   }
 </script>
 
-<button on:click={exportText} class='rounded-r inline-flex items-center bg-indigo-600 hover:bg-indigo-700 text-white px-2 py-2 active:bg-indigo-800 text-sm border border-indigo-700 mr-4' title="Export to text file" type="button">
+<button on:click={exportText} class='inline-flex items-center rounded bg-indigo-600 hover:bg-indigo-700 text-white px-2 py-2 active:bg-indigo-800 text-sm border border-indigo-700' title="Export to text file" type="button">
     Export to .txt File
 </button>
 
