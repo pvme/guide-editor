d138ba9d5a914025ff3d8620fa8465bf0a9de567
Rcm37 <46052381+Rcm37@users.noreply.github.com>
2023-09-24 12:09:53 -0400

    Implement .txt export button (#18)
    * Implement .txt export button
    
    * indent and button style
    
    * indent
    
    * indent
    
    * indent fix v3

---
+++ src/components/toolbar/ExportToTxt.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { text } from './../../stores'; 
+
+  function exportText() {
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:attachment/text,' + encodeURI($text);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'GuideEditorExport.txt';
+    hiddenElement.click();
+  }
+</script>
+
+<button on:click={exportText} class='inline-flex items-center rounded bg-indigo-600 hover:bg-indigo-700 text-white px-2 py-2 active:bg-indigo-800 text-sm border border-indigo-700' title="Export to text file" type="button">
+  Export to .txt File
+</button>
+
