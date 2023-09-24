48d511c3cebae3bf7af8cd4db995227d350bf492
Towsti <73656340+Towsti@users.noreply.github.com>
2023-09-24 20:43:45 +0200

    Update ExportToTxt.svelte to force site rebuild

--- src/components/toolbar/ExportToTxt.svelte
+++ src/components/toolbar/ExportToTxt.svelte
@@ -11,6 +11,6 @@
 </script>
 
 <button on:click={exportText} class='inline-flex items-center rounded bg-indigo-600 hover:bg-indigo-700 text-white px-2 py-2 active:bg-indigo-800 text-sm border border-indigo-700' title="Export to text file" type="button">
-  Export to .txt File
+  Export to .txt file
 </button>
 
