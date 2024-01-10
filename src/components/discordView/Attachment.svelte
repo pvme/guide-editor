472fda1bafa3f956a261b79c08b2b9a41a43e41f
Rcm37 <46052381+Rcm37@users.noreply.github.com>
2024-01-10 11:47:49 -0500

    Add webp extension for valid url (#25)
    Remove extension check on imgur urls

--- src/components/discordView/Attachment.svelte
+++ src/components/discordView/Attachment.svelte
@@ -5,10 +5,10 @@
 
     function urlToHTML(url) {
         let match;
-        if (url.match(/https?:\/\/i?\.?imgur\.com\/([a-zA-Z0-9]+)\.(png|jpg|jpeg|mp4)/))
+        if (url.match(/https?:\/\/i?\.?imgur\.com\/([a-zA-Z0-9]+)/))
             return `<img class='media' src='${url}'><p class='bg-red-900 p-2 my-2' style='color: hsla(0, 0%, 100%, .7)'>Warning! imgur links are now deprecated in PvME guides. Please replace this link with a pvme.io image link. For questions, contact any member of Editing Staff. <a href='${url}'>${url}</a></p>`;
         
-        if(url.match(/https?:\/\/img\.?pvme\.io\/images\/([a-zA-Z0-9]+)\.(png|jpg|jpeg|gif|mp4)/))
+        if(url.match(/https?:\/\/img\.?pvme\.io\/images\/([a-zA-Z0-9]+)\.(png|jpg|jpeg|gif|mp4|webp)/))
             return `<img class='media' src='${url}'>`;
         
         if (match = url.match(/https?:\/\/(?:youtu\.be\/|(?:www\.)?youtube\.com\/watch\?v=)([a-zA-Z0-9_\-]+)/)) {
